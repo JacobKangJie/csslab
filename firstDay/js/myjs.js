7a398ab69f052d7ddc791f9e2d018ea5cc861fe4
JacobKangJie <jiekang@thoughtworks.com>
2015-03-24 13:45:25 +0800

    final

--- firstDay/js/myjs.js
+++ firstDay/js/myjs.js
@@ -1,6 +1,6 @@
 $(function() {
-    var sWidth = $("#focus").width(); //获取焦点图的宽度（显示面积）
-    var sHeight = $("#focus").height();
+  //  var sWidth = $("#focus").width(); //获取焦点图的宽度（显示面积）
+    var sHeight = $("#focus").height(); //获取焦点图的高度
     console.log(sHeight);
     var len = $("#focus ul li").length; //获取焦点图个数
     var index = 0;
@@ -11,7 +11,7 @@ $(function() {
     for(var i=0; i < len; i++) {
         btn += "<span></span>";
     }
-    btn += "</div><div class='preNext pre'></div><div class='preNext next'></div>";
+    // btn += "</div><div class='preNext pre'></div><div class='preNext next'></div>";
     $("#focus").append(btn);
     $("#focus .btnBg").css("opacity",0.5);
 
@@ -28,19 +28,19 @@ $(function() {
         $(this).stop(true,false).animate({"opacity":"0.2"},300);
     });
 
-    //上一页按钮
-    $("#focus .pre").click(function() {
-        index -= 1;
-        if(index == -1) {index = len - 1;}
-        showPics(index);
-    });
-
-    //下一页按钮
-    $("#focus .next").click(function() {
-        index += 1;
-        if(index == len) {index = 0;}
-        showPics(index);
-    });
+    // //上一页按钮
+    // $("#focus .pre").click(function() {
+    //     index -= 1;
+    //     if(index == -1) {index = len - 1;}
+    //     showPics(index);
+    // });
+    //
+    // //下一页按钮
+    // $("#focus .next").click(function() {
+    //     index += 1;
+    //     if(index == len) {index = 0;}
+    //     showPics(index);
+    // });
 
     //本例为左右滚动，即所有li元素都是在同一排向左浮动，所以这里需要计算出外围ul元素的宽度
     // $("#focus ul").css("width",sWidth * (len));
@@ -65,8 +65,9 @@ $(function() {
     // }
     //显示图片函数，根据接收的index值显示相应的内容
     function showPics(index) { //普通切换
-        var nowLeft = -index*sHeight; //根据index值计算ul元素的left值
-        $("#focus ul").stop(true,false).animate({"bottom":nowLeft},300); //通过animate()调整ul元素滚动到计算出的position
+        var nowHeight = -index*sHeight; //根据index值计算ul元素的left值
+
+        $("#focus ul").stop(true,false).animate({"bottom":nowHeight},300); //通过animate()调整ul元素滚动到计算出的position
         $("#focus .btn span").stop(true,false).animate({"opacity":"0.4"},300).eq(index).stop(true,false).animate({"opacity":"1"},300);//为当前的按钮切换到选中的效果
     }
 });
